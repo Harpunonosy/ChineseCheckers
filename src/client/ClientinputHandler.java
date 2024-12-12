b5a7ab38a90857cdb5437a51249947b299fb6c9a
olgibolgi <279708@student.pwr.edu.pl>
2024-12-12 16:24:50 +0100

    adding logic ClientConnection, ClientOutputHandler and ClientInputHandler

--- src/client/ClientinputHandler.java
+++ src/client/ClientinputHandler.java
@@ -1,5 +1,19 @@
-package client;
+import java.util.Scanner;
 
-public class ClientinputHandler {
-    
+public class ClientInputHandler {
+    private ClientConnection connection;
+
+    public ClientInputHandler(ClientConnection connection) {
+        this.connection = connection;
+    }
+
+    public void handleInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter move (start-end): ");
+            String move = scanner.nextLine();
+            connection.sendMessage(move);
+        }
+    }
 }
