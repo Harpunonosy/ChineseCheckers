1a092b5be5b9f62096b038f5db198bb89b555691
olgibolgi <279708@student.pwr.edu.pl>
2024-12-12 15:13:22 +0100

    modify GameClinent class

--- src/client/GameClient.java
+++ src/client/GameClient.java
@@ -1,5 +1,23 @@
-package client;
+import java.io.IOException;
 
 public class GameClient {
-    
+    private ClientConnection connection;
+    private ClientInputHandler inputHandler;
+    private ClientOutputHandler outputHandler;
+
+    public GameClient(String serverAddress) throws IOException {
+        connection = new ClientConnection(serverAddress);
+        inputHandler = new ClientInputHandler(connection);
+        outputHandler = new ClientOutputHandler(connection);
+    }
+
+    public void start() {
+        new Thread(outputHandler).start();
+        inputHandler.handleInput();
+    }
+
+    public static void main(String[] args) throws IOException {
+        GameClient client = new GameClient("localhost");
+        client.start();
+    }
 }
