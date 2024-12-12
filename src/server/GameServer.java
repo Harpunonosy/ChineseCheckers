b29eccfd57a5534bf0b4697531dd071c12022586
Harpunonosy <279748@student.pwr.edu.pl>
2024-12-12 14:41:21 +0100

    Made shape of GameServer class

--- src/server/GameServer.java
+++ src/server/GameServer.java
@@ -1,5 +1,23 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameServer {
+    private List<PlayerHandler> players = new ArrayList<>();
+    private int playerCount;
+
+    public GameServer(int playerCount) {
+        this.playerCount = playerCount;
+    }
+
+    public static void main(String[] args) throws IOException {
+        GameServer server = new GameServer(2); // Example with 2 players
+        //server.startServer(); //TODO
+    }
+
     
 }
