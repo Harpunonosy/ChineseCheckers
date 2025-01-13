1cfcaee2cffabfd6adaf4041ee4086106ced5616
Harpunonosy <279748@student.pwr.edu.pl>
2025-01-13 23:57:50 +0100

    Add Move class + Make RuleSet outlines

--- src/rules/GameRuleSet.java
+++ src/rules/GameRuleSet.java
@@ -1,3 +1,6 @@
+import board.Board;
+
 public interface GameRuleSet {
-    
+  boolean isValidMove(Move move, int playerid, Board board);
+  boolean isGameOver(Board board);
 }
---
+++ src/rules/StandardRuleSet.java
@@ -0,0 +1,16 @@
+import board.Board;
+
+public class StandardRuleSet implements GameRuleSet{
+
+  @Override
+  public boolean isValidMove(Move move, int playerid, Board board){
+    //TODO
+    return true;
+  }
+
+  @Override
+  public boolean isGameOver(Board board){
+    //TODO
+    return true;
+  }
+}
