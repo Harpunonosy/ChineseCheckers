5269bc4ad4fe4b0f80d9721c80f5e02e8a6e340b
Harpunonosy <279748@student.pwr.edu.pl>
2025-01-13 23:38:49 +0100

    Make Board Interface and StandardBoard Outline

--- src/game/board/Board.java
+++ src/game/board/Board.java
@@ -1,5 +1,7 @@
 package board;
 
 public interface Board {
-    
+  void initialize();
+  void display();
+  void makeMove();  
 }
---
+++ src/game/board/StandardBoard.java
@@ -0,0 +1,20 @@
+package board;
+
+public class StandardBoard implements Board {
+
+  @Override
+  public void initialize(){
+    //TODO
+  }
+
+  @Override
+  public void display(){
+    //TODO
+  }
+
+  @Override
+  public void makeMove(){
+    //TODO
+  }
+
+}
